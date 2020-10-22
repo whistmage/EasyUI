67d6c886b0cbd0c3f8414de3491cd6744d339934
Whistmage <whistmage@gmail.com>
2020-10-22 20:27:50 +0700

    Add CustomView
    Добавлен протокол ViewCustomization, представляющий группу методов настройки view.
    Добавлен класс CustomView реализующий пртокол ViewCustomization для UIView.

---
+++ EasyUI/CustomView.swift
@@ -0,0 +1,52 @@
+//
+//  AbstractView.swift
+//  EasyUI
+//
+//  Created by Ivan Miroshnik on 22.10.2020.
+//  Copyright © 2020 whistmage. All rights reserved.
+//
+
+import UIKit
+
+public class CustomView: UIView, ViewCustomization {
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    public required init(center: CGPoint, width: CGFloat, height: CGFloat) {
+        let frame = CGRect.init(origin: center, size: CGSize.init(width: width, height: height))
+        super.init(frame: frame)
+        setupView()
+    }
+
+    public func addSubviws() {
+        
+    }
+
+    public func makeSubviewsConstraints() {
+        
+    }
+
+    public func configureView() {
+
+    }
+
+    public func configureSubviews() {
+        
+    }
+
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubviws()
+        makeSubviewsConstraints()
+        configureView()
+        configureSubviews()
+    }
+}
---
+++ EasyUI/ViewCustomization.swift
@@ -0,0 +1,14 @@
+//
+//  ViewCustomization.swift
+//  EasyUI
+//
+//  Created by Ivan Miroshnik on 22.10.2020.
+//  Copyright © 2020 whistmage. All rights reserved.
+//
+
+protocol ViewCustomization {
+    func addSubviws()
+    func makeSubviewsConstraints()
+    func configureSubviews()
+    func configureView()
+}
