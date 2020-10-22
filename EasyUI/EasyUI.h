97a39134ea168e0658f1de17832acc56ed17e9f3
Whistmage <whistmage@gmail.com>
2020-10-22 17:59:17 +0700

    Initial commit

---
+++ EasyUI/EasyUI.h
@@ -0,0 +1,19 @@
+//
+//  EasyUI.h
+//  EasyUI
+//
+//  Created by Ivan Miroshnik on 22.10.2020.
+//  Copyright © 2020 whistmage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyUI.
+FOUNDATION_EXPORT double EasyUIVersionNumber;
+
+//! Project version string for EasyUI.
+FOUNDATION_EXPORT const unsigned char EasyUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyUI/PublicHeader.h>
+
+
