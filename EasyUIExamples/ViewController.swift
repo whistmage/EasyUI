c46a8e188246ec1e5d2b973a220971e797021602
Whistmage <whistmage@gmail.com>
2020-10-22 18:14:52 +0700

    Add example project

---
+++ EasyUIExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyUIExamples
+//
+//  Created by Ivan Miroshnik on 22.10.2020.
+//  Copyright © 2020 whistmage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
